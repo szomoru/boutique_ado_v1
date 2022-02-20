850d75966b88e85c7eb041ec7e03d5c76ee95c9b
szomoru <gergely.vig1978@gmail.com>
2022-02-20 08:30:44 +0000

    Added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
@@ -15,34 +15,32 @@ def all_products(request):
     sort = None
     direction = None
 
-
     if request.GET:
-
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+           
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                message.error(request, "You didn't enter any search criteria!")
-                return redirect(reversed('products'))
-
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+           
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -51,7 +49,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current categories': categories,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
